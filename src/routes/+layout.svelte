d52228bda30d844a054b9795e6e5a542e1865634
Bofa Mpama <mpamabofa@gmail.com>
2025-08-26 10:51:56 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import Header from './Header.svelte';
+	import { onMount } from 'svelte';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+
+
+	<Header />
+	
+	<main>
+		{@render children()}
+	</main>
+
---
+++ src/routes/Header.svelte
@@ -0,0 +1,13 @@
+<script lang="ts"></script>
+<header>
+    <div class="logo">SoferBox</div>
+    <ul>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/writings">writings</a>
+        <a href="/contact">Contact</a>
+    </ul>
+</header>
+<style>
+    
+</style> 
