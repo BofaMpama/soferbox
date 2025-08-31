d51ae644ec1c63fce9b5580b81c781495a8e6533
Bofa Mpama <mpamabofa@gmail.com>
2025-08-31 07:35:01 +0100

    hamburger added

--- src/routes/RetroHeader.svelte
+++ src/routes/RetroHeader.svelte
@@ -1,4 +1,8 @@
-<script></script>
+
+<script>
+    import hamburger from '$lib/assets/burger.png';
+</script>
+
 <header class="flex justify-between gap-8 items-center px-6 py-3 bg-gray-100">
     <div class="logo text-5xl text-black">SoferBox</div>
     <nav class="text-xl  text-black flex gap-4">
@@ -7,10 +11,13 @@
         <a href="/about">About Me</a>
         <a href="/contact">Contact</a>
     </nav>
+      
    <div class="button-group">
+     <button><img src="{hamburger}" alt="Hamburger menu" class="burger"></button>
      <button class="border-black border-2 px-6 py-3 text-xl text-black btn">Sign Up</button>
     <button class="border-black border-2 px-6 py-3 text-xl text-black btn">Sign In</button>
    </div>
+
 </header>
 <style>
       header{
@@ -36,6 +43,14 @@ box-shadow: 4px 4px  rgb(0, 0, 0);
 color: white;
     }
 }
+.burger{
+    display: none;
+    width: 0;
+    flex: 0;
+    background: rgba(54, 112, 126, 0.664);
+    border: 2px solid black;
+    padding: 3px;
+}
   @media(max-width: 900px){
   
     nav{
@@ -46,6 +61,12 @@ color: white;
     }
     .btn{
         display: none;
+    }
+    .burger{
+        display: initial;
+        width: 50px;
+        height: 50px;
+        margin-left: auto;
     }
   }
 </style>
