6ea0b1c8ed16e47e2c2e1faf718003ebc2db6342
Bofa Mpama <mpamabofa@gmail.com>
2025-09-02 13:12:18 +0100

    Working on the poem page

--- src/routes/writings/poems/+page.svelte
+++ src/routes/writings/poems/+page.svelte
@@ -1,5 +1,59 @@
 <script lang="ts">
-    import type { PageProps } from './$types';
+    import { page } from "$app/stores";
+    import { onMount } from 'svelte';
 
-    let { data }: PageProps = $props();
+
+    type Poem = {
+        id: number;
+        Title: string;
+        Body: string;
+        Excerpt: string;
+        slug: string;
+        Featured: boolean;
+        PublishedDate: string;
+        createdAt: string;
+        updatedAt: string;
+        publishedAt: string;
+    };
+
+    let poem: Poem | null = null;
+    let slug: string;
+
+  
+
+    onMount(async () => {
+        const slug = $page.params.slug;
+
+        const res = await fetch(`http://localhost:1337/api/poems?filters[slug][$eq]=${slug}`);
+
+         const data = await res.json();
+
+       if(data.data.length > 0){
+         //Strapi returning an array in data.data
+        poem = {
+            id: data.data[0].id,
+            ...data.data[0].attributes
+
+        } as Poem;
+    
+       }
+        console.log("Fetched poem: ", poem);
+
+    });
+
+   
+
+
 </script>
+
+{#if poem}
+    <article class="poem">
+        <h1>{poem.Title}</h1>
+        <p><em>{poem.PublishedDate}</em></p>
+        <div class="poem-body">
+            {@html poem.Body.replace(/\n/g, "<br>")}
+        </div>
+    </article>
+    {:else}
+    <p>Loading data...</p>
+    {/if}
