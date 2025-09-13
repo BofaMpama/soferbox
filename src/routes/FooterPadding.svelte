330fde18437d787a346ac93020e55e0a3f87f60f
Bofa Mpama <mpamabofa@gmail.com>
2025-09-13 23:12:08 +0100

    footer scripture

--- src/routes/FooterPadding.svelte
+++ src/routes/FooterPadding.svelte
@@ -1,9 +1,30 @@
 <div class="footer-padding">
+    &OpenCurlyQuote;
+    Write a vision make it plain upon tablets, that he may run that readeth it. 
+    &CloseCurlyQuote; - Habakkuk 2:2
 </div>
 <style>
     .footer-padding{
-        min-height: 30dvh;
+        min-height: 50dvh;
+        display: flex;
+        justify-content: center;
+        align-items: first baseline;
         width: 100%;
         background: black;
+        color: white;
+        font-size: 40px;
+        font-weight: bold;
+        text-align: center;
+        padding: 20px;
+        padding-top: 40px;
+    }
+
+    @media(max-width: 600px){
+        .footer-padding{
+            align-items: first baseline;
+            min-height: 70dvh;
+            font-size: 30px;
+            padding: 30px;
+        }
     }
 </style>    
